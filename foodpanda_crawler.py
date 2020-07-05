9d7a841da898dd645d73944e398f6d41d38c6419
vbjc5275 <42717512+vbjc5275@users.noreply.github.com>
2020-07-05 12:09:38 +0800

    complete

--- foodpanda_crawler.py
+++ foodpanda_crawler.py
@@ -12,11 +12,8 @@ Created on Thu Jul  2 19:44:21 2020
 import requests
 from bs4 import BeautifulSoup,element
 import pandas as pd
-from selenium import webdriver
-import re
+from re import search
 import time
-from collections import namedtuple
-Point = namedtuple('Point', ['x', 'y'])
 # =============================================================================
 #   find the homepage of each city
 # =============================================================================
@@ -26,16 +23,11 @@ soup = BeautifulSoup(response.text)
 all_a = soup.find_all("a",class_="city-tile")
 all_link = [a.get("href") for a in all_a ]
 # =============================================================================
-#   foodpanda-find the all vendor
+#   foodpanda-find all vendor
 # =============================================================================
 url = "https://www.foodpanda.com.tw"+all_link[0]
 response = requests.get(url)
 soup = BeautifulSoup(response.text)
-all_vendor = soup.find_all("li")
-
-# =============================================================================
-#   foodpanda-find all vendor
-# =============================================================================
 all_li = soup.find("ul",class_="vendor-list").children
 all_vendor = []
 for v in all_li:
@@ -65,62 +57,59 @@ for vendor in all_vendor:
     except:
         v["tag"] = "NA"
     vendors.append(v)
-vendors_info = pd.DataFrame(vendor)
+    
+vendors_info = pd.DataFrame(vendors)
 vendors_info["id"] = [i for i in range(1,len(vendors_info)+1)]
                             
 # =============================================================================
 #  foodpanda - find menu
 # =============================================================================
 base_url = "https://www.foodpanda.com.tw"
-vendors_menu = []
+menus = []
 for id_,link,title in zip(vendors_info["id"].values,
                           vendors_info["link"].values,
                           vendors_info["name"].values):
-    print("title)
-    menu = {}
-    menu["id"] = id_
+    print("Crawling "+title)
     
     response = requests.get(base_url+link)
     soup = BeautifulSoup(response.text)
     
-    dishes = soup.find_all("div",class_="dish-category-section")
-    for dish in dishes:
+    dishes_section = soup.find_all("div",class_="dish-category-section")
+    for section in dishes_section:
         #restaurant menu-category
-        menu["category"] = dish.find("h2",class_="dish-category-title").text
-        #restaurant menu-name
-        menu["name"] = dish.find("h3").find("span").text
-        #menu-price
-        menu["price"] = dish.find("span",class_="price p-price").text.strip()
-    vendors_menu.append(menu)
-    time.sleep(0.5)
+        category = section.find("h2",class_="dish-category-title").text
+        dishes = section.find("ul",class_="dish-list").find_all("li")
+        for dish in dishes:
+            menu = {}
+            menu["id"] = id_
+            menu["category"] = category
+            #restaurant menu-name
+            menu["name"] = dish.find("h3").find("span").text
+            #menu-price
+            menu["price"] = dish.find("span",class_="price p-price").text.strip()
+            menus.append(menu)
+    time.sleep(0.3)
 
+vendors_menu = pd.DataFrame(menus)
 # =============================================================================
-#  ubereat
+# clean data
 # =============================================================================
-url = "https://www.ubereats.com/tw/feed?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMiVFOCU4NyVCQSVFNSU4QyU5NyVFOCVCQiU4QSVFNyVBQiU5OSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpVWi1XZlhLcFFqUVIwajRnZ1RvRDg5QSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0EyNS4wNDc2MzMzJTJDJTIybG9uZ2l0dWRlJTIyJTNBMTIxLjUxNjIzMjQlN0Q%3D"
-driver = webdriver.Chrome()
-driver.get(url)
-soup = BeautifulSoup(driver.page_source)
-soup.find_all("h3")
-soup.find_all("img")
+vendors_info[vendors_info.isnull()].sum()
+vendors_menu[vendors_menu.isnull()].sum()
 
-driver.find_element_by_css_selector('input[TEXT=myCheckBox]').click()
+def extract_number(str_num):
+    pattern = "[\d,]+.[\d]{2}"
+    return search(pattern, str_num).group(0)
 
-url = "https://www.ubereats.com/tw/feed?pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMiVFOCU4NyVCQSVFNSU4QyU5NyVFOCVCQiU4QSVFNyVBQiU5OSUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMkNoSUpVWi1XZlhLcFFqUVIwajRnZ1RvRDg5QSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJnb29nbGVfcGxhY2VzJTIyJTJDJTIybGF0aXR1ZGUlMjIlM0EyNS4wNDc2MzMzJTJDJTIybG9uZ2l0dWRlJTIyJTNBMTIxLjUxNjIzMjQlN0Q%3D"
-response = requests.get(url)
-soup = BeautifulSoup(response.text)
-soup
+vendors_menu.price = vendors_menu.price.apply(extract_number)
+vendors_menu.price = vendors_menu.price.apply(lambda price:price.replace(",",""))
 
-soup.find("div",class_="e8 g2 g3").find("img").find("img").get("src")
-soup.find_all(class_="e8 g2 g3")
-
-soup.find_all("h3")
-soup.find_all("img")
-
+vendors_info.count = vendors_info["count"].astype(int)
+vendors_menu.price = vendors_menu["price"].astype(float)
 # =============================================================================
-# 
+#  save data
 # =============================================================================
-url = "https://www.ubereats.com/tw/taipei/food-delivery/%E8%80%81%E8%B3%B4%E8%8C%B6%E6%A3%A7-%E5%8F%B0%E5%8C%97%E6%9D%BE%E5%B1%B1%E5%BA%97/pe-58e0bTC2RMb0othzLYQ"
-response = requests.get(url)
-soup = BeautifulSoup(response.text)
-soup.find_all("a")[15]
+with pd.ExcelWriter("food-panda.xlsx") as writer:
+    vendors_info.to_excel(writer,sheet_name="店家評分")
+    vendors_menu.to_excel(writer,sheet_name="店家菜單")
+    
